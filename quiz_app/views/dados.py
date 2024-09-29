34d96ab442c1dccbfed2a0a0d10d4a95b304970a
FelipeAugst <felipeaug92@gmail.com>
2024-09-29 13:42:00 -0300

    404 error handler

--- quiz_app/views/dados.py
+++ quiz_app/views/dados.py
@@ -1,7 +1,5 @@
 #base de dados para testarmos as funções 
-db = [{"nome":"felipe","email":"felipe@felipe","nick":"felipao","senha":"1234","id":1}, 
-           {"nome":"antonio","email":"antonio@antonio","senha":"1234","nick":"tonhao","id":2}, 
-           {"nome":"carlos","email":"carlos@carlos","senha":"1234","nick":"carlao","id":3}]
+db = []
 
 id=len(db)+1
 
